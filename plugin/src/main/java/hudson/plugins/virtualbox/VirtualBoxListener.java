2d5981003266fb7ba95663dfafa75e0439b84407
Benoit GUERIN <bguerin@netceler.com>
2013-10-24 16:09:31 +0200

    Protection from Null pointers

--- plugin/src/main/java/hudson/plugins/virtualbox/VirtualBoxListener.java
+++ plugin/src/main/java/hudson/plugins/virtualbox/VirtualBoxListener.java
@@ -1,5 +1,6 @@
 package hudson.plugins.virtualbox;
 
+import hudson.Extension;
 import hudson.model.TaskListener;
 import hudson.model.Computer;
 import hudson.model.Node;
@@ -10,24 +11,34 @@ import hudson.slaves.SlaveComputer;
 
 import java.io.Serializable;
 
+@Extension
 public class VirtualBoxListener extends RunListener<Run<?, ?>> implements Serializable {
 
     private static final long serialVersionUID = 5646627098165475986L;
 
     @Override
     public void onCompleted(final Run<?, ?> r, final TaskListener listener) {
-        final Computer c = r.getExecutor().getOwner();
-        if (c != null && c instanceof SlaveComputer) {
-            final SlaveComputer sc = (SlaveComputer) c;
-            final Node n = sc.getNode();
-            if (n != null && n instanceof VirtualBoxSlave) {
-                final VirtualBoxSlave slave = (VirtualBoxSlave) n;
-                if (slave.isRevertAfterBuild()) {
-                    sc.disconnect(OfflineCause.create(Messages._VirtualBoxListener_shuttingDownCause()));
-                    sc.connect(false);
+        final VirtualBoxTaskListenerLog log = new VirtualBoxTaskListenerLog(listener, "[VirtualBox] ");
+
+        if (r.getExecutor() != null && r.getExecutor().getOwner() != null) {
+            final Computer c = r.getExecutor().getOwner();
+            if (c != null && c instanceof SlaveComputer) {
+                log.logInfo("Run " + r.getDisplayName() + " completed on computer " + c.getDisplayName()
+                        + " which is a VirtualBox slave.");
+                final SlaveComputer sc = (SlaveComputer) c;
+                final Node n = sc.getNode();
+                if (n != null && n instanceof VirtualBoxSlave) {
+                    final VirtualBoxSlave slave = (VirtualBoxSlave) n;
+                    if (slave.isRevertAfterBuild()) {
+                        log.logInfo("Reverting slave " + slave.getDisplayName()
+                                + " to configured snapshot, as requested per configuration of this slave.");
+                        sc.disconnect(OfflineCause.create(Messages._VirtualBoxListener_shuttingDownCause()));
+                        sc.connect(false);
+                    }
                 }
             }
         }
         super.onCompleted(r, listener);
     }
+
 }
